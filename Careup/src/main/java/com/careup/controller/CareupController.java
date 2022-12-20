5419f3821d6a85be5d3a7aca8a2d8cfde0951ef5
Dev_Rajput_4 <dklodhi4@gmail.com>
2022-12-20 15:30:55 +0530

    CrossOrigin annotation added in the controller

--- Careup/src/main/java/com/careup/controller/CareupController.java
+++ Careup/src/main/java/com/careup/controller/CareupController.java
@@ -10,9 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api")
-@CrossOrigin("http://localhost:4200")
 public class CareupController {
     @Autowired
     private CareupService careupService;
