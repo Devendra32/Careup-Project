31e96e8f81acae816fc9637263f6c7bd8bd83974
Dev_Rajput_4 <dklodhi4@gmail.com>
2022-12-23 17:47:34 +0530

    validation complete for user form input fields

--- Careup_GUI/src/app/careup/user/user.component.ts
+++ Careup_GUI/src/app/careup/user/user.component.ts
@@ -17,10 +17,10 @@ export class UserComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.getRoles();
+    // this.getRoles();
   }
-  onSubmit() {
-    console.log(this.user.role);
+  onSubmit(data:any) {
+    console.log(data);
     // this.saveUser();
   }
   saveUser() {
@@ -37,6 +37,12 @@ export class UserComponent implements OnInit {
       this.roles = data;
     });
   }
- 
+  numberOnly(event: { which: any; keyCode: any; }): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+  }
 
 }
